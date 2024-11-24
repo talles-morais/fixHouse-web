a1743696a3f13bdf0d6de6603d26ca097a393ba3
Talles <talves2503@gmail.com>
2024-11-24 09:58:49 -0300

    feat: map embed and client support button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,11 @@
 import PopularServices from "@/components/PopularServices";
 import SearchInput from "@/components/SearchInput";
 import Image from "next/image";
-import GoogleMap from "@/services/map";
+import { AddressMap } from "@/services/map";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-5">
+    <div className="flex flex-col justify-center items-center min-h-screen gap-5 pt-10 pb-20">
       <Image 
         width={20}
         height={30}
@@ -16,8 +16,22 @@ export default function Home() {
 
       <SearchInput />
       <PopularServices />
-      <GoogleMap address="Itajubá " />
+      <AddressMap />
+      <button className="bg-dark-blue py-4 px-6 text-white max-w-[264px] rounded-lg">
+        <div className="flex gap-7 ">
+          <Image 
+            width={44}
+            height={44}
+            src={"/icons/sac.svg"}
+            alt="Ícone de atendente/suporte"
+          />
 
+          <div className="flex flex-col ">
+            <h2 className="text-sm font-bold text-left">Suporte ao cliente</h2>
+            <p className="text-[10px] text-left">Nossa equipe de suporte está pronta para ajudar você a resolver qualquer questão de forma rápida e eficiente.</p>
+          </div>
+        </div>
+      </button>
     </div>
   )
 }
--- src/services/map.tsx
+++ src/services/map.tsx
@@ -1,52 +1,13 @@
-"use client";
+const AddressMap = () => {
+  return (
+    <div>
+      <img
+        src="https://maps.googleapis.com/maps/api/staticmap?center=-22.4196,-45.4359&zoom=10&size=272x128&key=AIzaSyAgXiatzxayUzZnR6H33ucGjo5uFEnPx-E"
+        alt="Mapa de localização"
+        style={{ width: "272px", height: "128px" }}
+      />
+    </div>
+  );
+};
 
-import { useEffect, useRef, useState } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
-
-interface GoogleMapProps {
-  address: string;
-}
-
-function GoogleMap({ address }: GoogleMapProps): JSX.Element {
-  const mapRef = useRef<HTMLDivElement | null>(null); // Ref inicializada como HTMLDivElement | null
-  const [isApiLoaded, setIsApiLoaded] = useState(false);
-
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-      version: "weekly",
-    });
-
-    loader.load().then(() => {
-      setIsApiLoaded(true);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!isApiLoaded || !mapRef.current) return; // Certifique-se de que o mapRef não é null
-
-    const geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === google.maps.GeocoderStatus.OK && results && results[0]) {
-        const map = new google.maps.Map(mapRef.current as HTMLElement, {
-          center: results[0].geometry.location,
-          zoom: 8,
-        });
-
-        new google.maps.Marker({
-          map,
-          position: results[0].geometry.location,
-        });
-      } else {
-        console.error(
-          `Geocode was not successful for the following reason: ${status}`
-        );
-      }
-    });
-  }, [address, isApiLoaded]);
-
-  return <div style={{ height: "400px" }} ref={mapRef} />;
-}
-
-export default GoogleMap;
+export { AddressMap };
